f5929300b76603af6425df853a30e43458757348
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2021-01-12 00:28:17 -0500

    testing query method

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -7,9 +7,14 @@
                 <small>Subheading</small>
             </h1>
 
-            <?php if($database->connection){
-                 echo "true";
-            } ?>
+            <?php 
+            
+            $sql = "SELECT * FROM users WHERE id=1";
+            $result = $database->query($sql);
+            $user_found = mysqli_fetch_array($result);
+            echo $user_found['username'];
+            
+            ?>
 
             <ol class="breadcrumb">
                 <li>
